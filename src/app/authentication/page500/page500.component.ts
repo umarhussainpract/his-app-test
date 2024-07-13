4f74ce7023d8d927a8870d1c87a6fc870deb4158
umarhussainpract <umarulla.hussainy@practner.com>
2024-07-13 10:16:48 +0400

    first commit

---
+++ src/app/authentication/page500/page500.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+@Component({
+    selector: 'app-page500',
+    templateUrl: './page500.component.html',
+    styleUrls: ['./page500.component.scss'],
+    standalone: true,
+    imports: [
+        FormsModule,
+        MatButtonModule,
+        RouterLink,
+    ],
+})
+export class Page500Component {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() { }
+}
